aef473e62f7c252bb306266b3e657c2908c0e851
Gregory Vincic <g@7de.se>
2021-07-23 09:39:38 +0200

    Add more examples

--- example_test.go
+++ example_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gregoryv/vars"
 )
 
-func ExampleCopy() {
+func ExampleCopyAll() {
 	var (
 		i int
 		s string
@@ -24,4 +24,21 @@ func ExampleCopy() {
 	// output:
 	// Copy[5]: not int
 	// Copy[7]: not string
+}
+
+func ExampleCopy() {
+	var (
+		i int
+		s string
+	)
+	pairs := []interface{}{
+		&i, 1, // ok
+		&s, "a", // ok
+		&i, "b", // bad
+		&s, 0, // bad
+	}
+	err := vars.Copy(pairs...)
+	fmt.Println(err)
+	// output:
+	// Copy[5]: not int
 }
